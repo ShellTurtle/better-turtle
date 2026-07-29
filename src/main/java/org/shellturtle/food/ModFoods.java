3ac4a1d905e1794ac096ab46083216771b3fe24f
ShellTurtle <ShellTurtle2024@outlook.com>
2026-07-30 07:54:11 +0800

    初始版本

---
+++ src/main/java/org/shellturtle/food/ModFoods.java
@@ -0,0 +1,7 @@
+package org.shellturtle.food;
+
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoods {
+    public static final FoodProperties TURTLE_JELLY = new FoodProperties.Builder().nutrition(4).saturationModifier(0.2F).alwaysEdible().build();
+}
---
+++ src/main/java/org/shellturtle/item/ModItems.java
@@ -0,0 +1,47 @@
+package org.shellturtle.item;
+
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.minecraft.data.recipes.CustomCraftingRecipeBuilder;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import org.shellturtle.food.ModFoods;
+
+import net.minecraft.world.item.BlockItem;
+import org.shellturtle.BetterTurtle;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static final Item TURTLE_JELLY = registerItem("turtle_jelly",Item::new , new Item.Properties().food(ModFoods.TURTLE_JELLY));
+
+    private static Item registerItem(final String name, final Function<Item.Properties, Item> itemFactory, final Item.Properties properties) {
+        ResourceKey<Item> key = ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(BetterTurtle.MOD_ID, name));
+        Item item = itemFactory.apply(properties.setId(key));
+        if (item instanceof BlockItem blockItem) {
+            blockItem.registerBlocks(Item.BY_BLOCK, item);
+        }
+
+        return Registry.register(BuiltInRegistries.ITEM, key, item);
+    }
+
+    private static Item registerItem(final String name, final Function<Item.Properties, Item> ItemFactory) {
+        return  registerItem(name, ItemFactory, new Item.Properties());
+    }
+
+    private static Item registerItem(final String name) {
+        return  registerItem(name, Item::new, new Item.Properties());
+    }
+
+    public static void register() {
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.FOOD_AND_DRINKS).register(fabricCreativeModeTabOutput -> {
+            fabricCreativeModeTabOutput.accept(TURTLE_JELLY);
+        });
+    }
+}
